65d2134e531f8c016ff46d6a0678b08017045f74
cnoom <1195582817@qq.com>
2025-03-15 09:42:37 +0800

    ActionManager.cs 优化内部实现，新增update注册

--- Runtime/Modules/ActionModule/ActionManager.cs
+++ Runtime/Modules/ActionModule/ActionManager.cs
@@ -18,15 +18,42 @@ namespace CnoomFrameWork.Modules.ActionModule
         // 优先队列（按时间排序）
         private readonly PriorityQueue<DelayTask> timeQueue = new PriorityQueue<DelayTask>((a, b) => a.ExecuteTime.CompareTo(b.ExecuteTime));
 
+        private Action update;
+
+        protected override void OnInitialize()
+        {
+            App app = App.Instance;
+            GameObject actionGo = new GameObject("ActionManager");
+            actionGo.transform.SetParent(app.transform);
+
+            GameObject timeGo = new GameObject("TimeTimer");
+            timeGo.transform.SetParent(actionGo.transform);
+            timeGo.AddComponent<UpdateMono>().UpdateAction = UpdateTime;
+
+            GameObject frameGo = new GameObject("FrameTimer");
+            frameGo.transform.SetParent(actionGo.transform);
+            frameGo.AddComponent<UpdateMono>().UpdateAction = UpdateFrame;
+
+            GameObject updateGo = new GameObject("UpdateTimer");
+            updateGo.transform.SetParent(actionGo.transform);
+            updateGo.AddComponent<UpdateMono>().UpdateAction = Update;
+        }
+
         private void Update()
         {
-            // 处理时间队列
+            update?.Invoke();
+        }
+
+        private void UpdateTime()
+        {
             while (timeQueue.Count > 0 && timeQueue.Peek().ExecuteTime <= Time.time)
             {
                 ExecuteTask(timeQueue.Dequeue());
             }
+        }
 
-            // 处理帧队列
+        private void UpdateFrame()
+        {
             while (frameQueue.Count > 0 && frameQueue.Peek().ExecuteFrame <= Time.frameCount)
             {
                 ExecuteTask(frameQueue.Dequeue());
@@ -37,6 +64,16 @@ namespace CnoomFrameWork.Modules.ActionModule
         {
             task.Execute();
             task.Recycle();
+        }
+
+        public void RegisterUpdate(Action callback)
+        {
+            update += callback;
+        }
+
+        public void UnRegisterUpdate(Action callback)
+        {
+            update -= callback;
         }
 
         public DelayTask DelaySeconds(float seconds, Action callback)
@@ -75,6 +112,15 @@ namespace CnoomFrameWork.Modules.ActionModule
         internal void PushToPool(DelayTask task)
         {
             taskPool.Push(task);
+        }
+
+        internal class UpdateMono : MonoBehaviour
+        {
+            public Action UpdateAction;
+            private void Update()
+            {
+                UpdateAction?.Invoke();
+            }
         }
     }
 }
