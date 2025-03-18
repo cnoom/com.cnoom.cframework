4d83c96c95ab4b0d110d01bc8928933011ecff5b
cnoom <1195582817@qq.com>
2025-03-18 13:22:58 +0800

    修复资源地址错误计数

--- Editor/Addressable/AddressableNameSimplifier.cs
+++ Editor/Addressable/AddressableNameSimplifier.cs
@@ -9,8 +9,7 @@ namespace FrameWork.Editor.Addressable
     public static class AddressableNameSimplifier
     {
 
-        // 冲突解决计数器
-        private static readonly Dictionary<string, int> _conflictTracker = new Dictionary<string, int>();
+     
 
         /// <summary>
         ///     生成基于文件名的简化地址
@@ -21,14 +20,7 @@ namespace FrameWork.Editor.Addressable
 
             // 步骤1：清理文件名
             string cleanedName = NamingValidator.SanitizeVariableName(fileName);
-
-            // 步骤2：冲突解决
-            if(_conflictTracker.ContainsKey(cleanedName))
-            {
-                _conflictTracker[cleanedName]++;
-                return $"{cleanedName}_{_conflictTracker[cleanedName]}";
-            }
-            _conflictTracker[cleanedName] = 0;
+            
             return cleanedName;
         }
     }
