c67f890426972e965bfe00294a51e7d53c53eeb3
cnoom <1195582817@qq.com>
2025-03-24 15:19:36 +0800

    新增ICollection接口拓展随机排序拓展方法

---
+++ Runtime/Extensions/ICollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CnoomFrameWork.Extensions
+{
+    public static class ICollectionExtensions
+    {
+        /// <summary>
+        ///  Fisher-Yates 随机排序
+        /// </summary>
+        /// <param name="self"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void ShuffleExtension<T>(this ICollection<T> self)
+        {
+            System.Random rand = new System.Random();
+            T[] array = self.ToArray();
+            int n = array.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int r = i + rand.Next(n - i);
+                (array[r], array[i]) = (array[i], array[r]);
+            }
+            self.Clear();
+            foreach (T item in array)
+            {
+                self.Add(item);
+            }
+        }
+    }
+}
