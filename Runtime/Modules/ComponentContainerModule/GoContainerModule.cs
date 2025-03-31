bd1d6d20492d738d263b38fac6612d85d1f40ffb
cnoom <1195582817@qq.com>
2025-03-31 09:27:36 +0800

    GoContainerModule.cs 增加动态增删

--- Runtime/Modules/ComponentContainerModule/GoContainerModule.cs
+++ Runtime/Modules/ComponentContainerModule/GoContainerModule.cs
@@ -9,22 +9,29 @@ namespace Modules.ComponentContainerModule
 {
     public class GoContainerModule : Module
     {
-        private Dictionary<string,Component> components = new Dictionary<string, Component>();
+        private Dictionary<string, Component> components = new Dictionary<string, Component>();
 
         public bool HasComponent(string key) => components.ContainsKey(key);
-        
-        public T GetComponent<T>(string key) where T : Component => components[key] is T ? (T) components[key] : GetGameObject(key).GetComponent<T>();
-        
+
+        public T GetComponent<T>(string key) where T : Component => components[key] is T ? (T)components[key] : GetGameObject(key).GetComponent<T>();
+
         public GameObject GetGameObject(string key) => components[key].gameObject;
         
+        public void RemoveComponent(string key) => components.Remove(key);
+
+        public void RegisterComponent(string key, Component component)
+        {
+            if(components.ContainsKey(key))
+            {
+                throw new Exception($"同名 {key} 已经存在,为 {components[key].GetType()}类型");
+            }
+            components.Add(key, component);
+        }
+
         [Subscribe, Preserve]
         private void OnRegisterComponent(EventRegisterComponent e)
         {
-            if(components.ContainsKey(e.Key))
-            {
-                throw new Exception($"组件 {e.Key} 已经存在");
-            }    
-            components.Add(e.Key,e.Component);
+            RegisterComponent(e.Key, e.Component);
         }
 
         [Subscribe, Preserve]
@@ -32,13 +39,13 @@ namespace Modules.ComponentContainerModule
         {
             components.Remove(e.Key);
         }
-        
+
         public struct EventRegisterComponent
         {
             public string Key;
             public Component Component;
         }
-        
+
         public struct EventUnRegisterComponent
         {
             public string Key;
