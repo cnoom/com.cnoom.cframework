4c49f22f3894c21177c4e8961352372459b9e0f8
cnoom <1195582817@qq.com>
2025-06-05 15:17:38 +0800

    移除App的异常捕获功能

--- Runtime/App.cs
+++ Runtime/App.cs
@@ -2,7 +2,6 @@ using CnoomFrameWork.Base.Config;
 using CnoomFrameWork.Base.Log;
 using CnoomFrameWork.Singleton;
 using CnoomFrameWork.Base.Container;
-using UnityEngine;
 
 namespace CnoomFrameWork.Core
 {
@@ -14,36 +13,12 @@ namespace CnoomFrameWork.Core
         public ILog Log { get; private set; }
         public RootContainer RootContainer { get; private set; }
 
-        private void OnDestroy()
-        {
-            Application.logMessageReceived -= OnHandleException;
-        }
-
-        /// <summary>
-        ///     全局异常处理回调
-        /// </summary>
-        /// <param name="condition">异常信息内容</param>
-        /// <param name="stackTrace">异常调用堆栈</param>
-        /// <param name="type">Unity日志类型</param>
-        private void OnHandleException(string condition, string stackTrace, LogType type)
-        {
-            switch(type)
-            {
-                case LogType.Log:
-                case LogType.Assert:
-                    return;
-            }
-            var message = $"{condition}\n{stackTrace}";
-            Log.LogWithSender(nameof(App), message);
-        }
-
         /// <summary>
         ///  入口方法
         /// </summary>
         public static void Boot()
         {
             App app = Instance;
-            Application.logMessageReceived += app.OnHandleException;
         }
 
         /// <summary>
