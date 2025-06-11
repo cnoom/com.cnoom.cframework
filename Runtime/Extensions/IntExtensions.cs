5239a36d02b57d028e2fcc797de0eecc5731d0e9
cnoom <1195582817@qq.com>
2025-06-11 09:47:31 +0800

    新增整数拓展方法:生成一个0到该整数-1的可枚举序列

---
+++ Runtime/Extensions/IntExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CnoomFrameWork.Extensions
+{
+    public static class IntExtensions
+    {
+        /// <summary>
+        /// 生成从0到当前整数-1的序列
+        /// </summary>
+        /// <param name="count">序列长度</param>
+        /// <returns>从0开始的整数序列</returns>
+        public static IEnumerable<int> ToSequence(this int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return i;
+            }
+        }
+    }
+}
